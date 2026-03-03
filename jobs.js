0c34aff54d37080401c291f9284fc43c74cbf273
Masud Rana <masud@MacBook-Pro.local>
2026-03-03 14:42:49 +0600

    added js code

--- jobs.js
+++ jobs.js
@@ -1,21 +1,8 @@
 
+let currentTab = 'all';
 
-
-// dashBord count elements
-let totlaCount = document.getElementById('total');
-let interviewCount = document.getElementById('interviewCount');
-let rejectedCount = document.getElementById('rejwectedCount');
- let totalCount2 = document.getElementById('total1');
-//console.log(totlaCount);
-
-const allCardssection = document.getElementById('allCards');
-
-///console.log(allCards.children.length);
-
-function calculateCount() {
-
-    totlaCount.innerText = allCards.children.length;
-    totalCount2.innerText = allCards.children.length;
+function switchTab(tab){
+    //console.log(tab);
+    const tabs = ["all", "interview", "rejected"];
 }
 
-calculateCount(); 
