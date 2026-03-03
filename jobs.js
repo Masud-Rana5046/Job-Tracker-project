54f6fe58f75752c8ca7e0f4f300e0de7aaae9826
Masud Rana <masud@MacBook-Pro.local>
2026-03-03 14:58:26 +0600

    added function on button

--- jobs.js
+++ jobs.js
@@ -1,8 +1,23 @@
 
 let currentTab = 'all';
 
+const tabActive = ["bg-[#0000f8]", "border-[#0000f8]", "text-white"];
+const tabInactive = ["bg-white", "border-white", "text-black"];
+
 function switchTab(tab){
     //console.log(tab);
     const tabs = ["all", "interview", "rejected"];
+    for(const t of tabs){
+        const tabName = document.getElementById("tab-" + t);
+        //console.log(tabName);
+        if(t === tab){
+            tabName.classList.remove(...tabInactive);
+            tabName.classList.add(...tabActive);
+        }
+        else{
+            tabName.classList.add(...tabInactive);
+            tabName.classList.remove(...tabActive);
+        }
+    }
 }
 
